4482877b79af71c6e5334bf3844d4b6196f70f47
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-02-23 00:05:25 +0530

    Time: 0 ms (100%), Space: 8 MB (44.66%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        
+        int last = -1;     
+        int maxDist = 0;   
+        int pos = 0;       
+        
+        while (n > 0) {
+            
+            if (n & 1) {   
+                
+                if (last != -1) {
+                    maxDist = max(maxDist, pos - last);
+                }
+                
+                last = pos;
+            }
+            
+            n = n >> 1;   
+            pos++;
+        }
+        
+        return maxDist;
+    }
+};
