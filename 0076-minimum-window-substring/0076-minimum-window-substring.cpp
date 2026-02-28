6624464aeee1a95bede50487842d737809f7d68f
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-03-01 00:22:32 +0530

    Time: 4 ms (71.06%), Space: 11.4 MB (63.95%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+       if (s.size()<t.size()){
+        return "" ;
+       } 
+       vector<int>freq(128,0);
+       for(char c:t){
+        freq[c]++;
+       }
+       int left = 0;
+       int right = 0;
+       int start = 0;
+       int count = t.size();
+       int minLen = INT_MAX;
+
+       while(right<s.size()){
+        if (freq[s[right]] > 0)
+        count --;
+
+        freq[s[right]]--;
+        right++;
+
+        while (count == 0){ // window valid
+        if (right-left < minLen){
+            minLen = right - left;
+            start =left;
+        }
+
+        freq[s[left]]++; // window shrink kardo
+        if(freq[s[left]] > 0){
+            count ++;
+            
+        }
+             left++;
+        }
+        }
+        return minLen == INT_MAX ? "" : s.substr(start , minLen);
+       }
+    
+};
