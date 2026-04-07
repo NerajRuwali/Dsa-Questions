2872b9d5115506edb5551eee0160e51b8a2934d5
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-04-07 19:56:38 +0530

    Time: 30 ms (65.52%), Space: 127.3 MB (93.1%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -0,0 +1,48 @@
+class Robot {
+public:
+    int w, h;
+    int x = 0, y = 0;
+    int dir = 0; // 0=East, 1=North, 2=West, 3=South
+    int perimeter;
+
+    Robot(int width, int height) {
+        w = width;
+        h = height;
+        perimeter = 2 * (w + h - 2);
+    }
+
+    void step(int num) {
+        num %= perimeter;
+        if(num == 0) num = perimeter;
+
+        while(num--) {
+            if(dir == 0) { // East
+                if(x + 1 < w) x++;
+                else { dir = 1; y++; }
+            }
+            else if(dir == 1) { // North
+                if(y + 1 < h) y++;
+                else { dir = 2; x--; }
+            }
+            else if(dir == 2) { // West
+                if(x - 1 >= 0) x--;
+                else { dir = 3; y--; }
+            }
+            else { // South
+                if(y - 1 >= 0) y--;
+                else { dir = 0; x++; }
+            }
+        }
+    }
+
+    vector<int> getPos() {
+        return {x, y};
+    }
+
+    string getDir() {
+        if(dir == 0) return "East";
+        if(dir == 1) return "North";
+        if(dir == 2) return "West";
+        return "South";
+    }
+};
