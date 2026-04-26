bfa86566f9fd8e41b9262ee0315cbb4b92a33c38
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-04-26 09:46:36 +0530

    Time: 0 ms (100%), Space: 20.5 MB (60.4%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -7,14 +7,12 @@ public:
         int farthest = 0;
 
         for(int i = 0; i < n - 1; i++) {
-            
-            // Update farthest reachable index
+    
             farthest = max(farthest, i + nums[i]);
 
-            // If we reach end of current range
             if(i == currentEnd) {
-                jumps++;                 // Make a jump
-                currentEnd = farthest;   // Update new range
+                jumps++;                 
+                currentEnd = farthest;   
             }
         }
 
