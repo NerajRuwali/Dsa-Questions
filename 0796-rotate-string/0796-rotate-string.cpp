ea7e1c2b9f49538c07432114b880ddf4adfbc849
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-05-03 17:49:44 +0530

    Time: 0 ms (100%), Space: 8.3 MB (36.38%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
-
-        string temp = s + s;
-
-        return temp.find(goal) != string::npos;
+         string temp = s + s;
+         return temp.find(goal) != string::npos;
     }
 };
