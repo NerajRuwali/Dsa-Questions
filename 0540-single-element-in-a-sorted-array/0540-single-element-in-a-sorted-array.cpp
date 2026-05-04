21a83905cd34bc033717a61e00cf3cf8a0af3689
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-05-04 18:19:53 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.96%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,17 +5,13 @@ public:
 
         while (left < right) {
             int mid = left + (right - left) / 2;
-
-            
-            if (mid % 2 == 1) mid--;
-
-            if (nums[mid] == nums[mid + 1]) {
+           if (mid % 2 == 1) mid--;
+          if (nums[mid] == nums[mid + 1]) {
                 left = mid + 2;
             } else {
                 right = mid;
             }
         }
-
-        return nums[left];
+     return nums[left];
     }
 };
