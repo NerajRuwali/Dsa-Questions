83bc356c9d69821c50bbd8cc19ae73c66a2b10a4
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-05-17 08:55:40 +0530

    Time: 3 ms (45.57%), Space: 39.6 MB (35.05%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    bool dfs(vector<int>& arr, int i, vector<bool>& visited){
+
+        if(i < 0 || i >= arr.size() || visited[i]){
+            return false;
+        }
+
+        if(arr[i] == 0){
+            return true;
+        }
+
+        visited[i] = true;
+
+        return dfs(arr, i + arr[i], visited) ||
+               dfs(arr, i - arr[i], visited);
+    }
+
+    bool canReach(vector<int>& arr, int start) {
+
+        vector<bool> visited(arr.size(), false);
+
+        return dfs(arr, start, visited);
+    }
+};
