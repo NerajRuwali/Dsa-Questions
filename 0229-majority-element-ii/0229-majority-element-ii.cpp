8ff08d99c5be3b99e69aa00669b5a70b94f60116
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-06-12 19:28:57 +0530

    Time: 0 ms (100%), Space: 23.6 MB (81.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,9 +1,8 @@
  class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
         int n = nums.size();
-        int candidate1 = -1, candidate2 = -1;
+        int candidate1 = -1, candidate2 =-1;
         int count1 = 0, count2 = 0;
         for(int num : nums){
             if(num == candidate1){
@@ -12,15 +11,15 @@ public:
             else if(num == candidate2){
                 count2++;
             }
-            else if(count1 == 0){
-                candidate1 = num;   
+            else if ( count1== 0){
+                candidate1 = num;
                 count1 = 1;
             }
-            else if(count2 == 0){
-                candidate2 = num;   
+            else if (count2 == 0){
+                candidate2 = num;
                 count2 = 1;
             }
-            else{
+            else {
                 count1--;
                 count2--;
             }
@@ -29,15 +28,18 @@ public:
         count2 = 0;
 
         for(int num : nums){
-            if(num == candidate1) count1++;
-            else if(num == candidate2) count2++;   
+            if( num == candidate1) count1++;
+            else if (num == candidate2) count2++;
         }
 
         vector<int> answer;
 
-        if(count1 > n/3) answer.push_back(candidate1);
-        if(count2 > n/3) answer.push_back(candidate2);
+        if(count1> n/3) answer.push_back(candidate1);
+        if(count2> n/3) answer.push_back(candidate2);
 
         return answer;
     }
-};
+ };
+
+
+        
