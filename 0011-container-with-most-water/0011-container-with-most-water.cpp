743cd5d24209397bdc29165bee6865df00454b19
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-06-20 12:07:35 +0530

    Time: 0 ms (100%), Space: 62.8 MB (97.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,7 +3,7 @@ public:
     int maxArea(vector<int>& height) {
         int left = 0;
         int right = height.size()-1;
-        int maxwater = 0;
+        int maxwater =0;
         while(left<right){
             int width = right - left;
             int h = min(height[left], height[right]);
