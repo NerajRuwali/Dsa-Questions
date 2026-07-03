1e0cb14e52c6fcc8acb70fa205a3286b8b7dfd31
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-03 11:11:10 +0530

    Time: 24 ms (9.74%), Space: 28.4 MB (63.21%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
 
         int n = nums.size();
-
         vector<int> ans(n, -1);
-
         stack<int> st;
 
         for (int i = 2 * n - 1; i >= 0; i--) {
