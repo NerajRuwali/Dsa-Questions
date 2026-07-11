e91b90a4b36e0e4eebe20813e2b8d3d92545d9de
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-11 23:13:58 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 2) return n;
+     if(n<=2) return n;
 
-        int step2 = 1; 
-        int step1 = 2; 
+     int step2 = 1;
+     int step1 = 2;
 
-        for(int i = 3; i <= n; i++) {
-            int curr = step1 + step2;
-            step2 = step1;
-            step1 = curr;
-        }
-return step1;
+     for(int i = 3; i<=n ; i++){
+        int curr = step1 + step2;
+        step2 = step1;
+        step1 = curr;
+     }
+     return step1;
     }
 };
+
+       
