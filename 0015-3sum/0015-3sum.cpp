3015739d01be4e913c61d1c2bf3ae83689539a59
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-11 10:52:19 +0530

    Time: 46 ms (69.13%), Space: 29.1 MB (47.58%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,24 +4,22 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
 
-        for(int i = 0; i < nums.size(); i++){
-            
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-             int j = i + 1;
-            int k = nums.size() - 1;
+        for(int i = 0; i<nums.size(); i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j<k){
+                int sum = nums[i]+ nums[j] + nums[k];
 
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k] ;
-
-                if(sum == 0) {
+                if(sum==0){
                     result.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
 
-                    while(j < k && nums[j] == nums[j - 1]) j++;
-                    while(j < k && nums[k] == nums[k + 1]) k--;
+                    while(j<k && nums[j]== nums[j-1])j++;
+                    while(j<k && nums[k]== nums[k+1])k--;
                 }
-                else if(sum < 0){
+                else if(sum<0){
                     j++;
                 }
                 else{
@@ -29,7 +27,8 @@ public:
                 }
             }
         }
-
         return result;
     }
 };
+
+       
