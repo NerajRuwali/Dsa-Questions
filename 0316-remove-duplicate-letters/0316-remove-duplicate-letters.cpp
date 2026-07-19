438cd3ee63d3cd4404871094de0d96089e1d4f64
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-19 13:52:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (17.91%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> last(256, -1);
+
+        // Store last occurrence of every character
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i]] = i;
+        }
+
+        stack<char> st;
+        vector<bool> vis(256, false);
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            // Skip if already present
+            if (vis[ch])
+                continue;
+
+            // Remove larger characters if they appear later
+            while (!st.empty() &&
+                   ch < st.top() &&
+                   last[st.top()] > i) {
+                vis[st.top()] = false;
+                st.pop();
+            }
+
+            st.push(ch);
+            vis[ch] = true;
+        }
+
+        string ans = "";
+
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
