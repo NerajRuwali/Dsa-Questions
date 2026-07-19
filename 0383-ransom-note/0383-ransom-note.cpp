573a92daccfa08f60e526338649509b3bc4bae63
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-19 14:16:55 +0530

    Time: 3 ms (52.73%), Space: 11.9 MB (49.37%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char , int> m;
+    bool canConstruct(string ransomeNote, string magazine) {
+        unordered_map<char , int>m;
 
         for(int i = 0 ; i< magazine.size() ; i++){
             m[magazine[i]]++;
         }
-        for (int i = 0 ; i< ransomNote.size() ; i++){
-            if(m[ransomNote[i]] > 0){
-                m[ransomNote[i]]--;
-            }
-            else {
+        for(int i = 0 ; i < ransomeNote.size() ; i++){
+            if(m[ransomeNote[i]] > 0){
+                m[ransomeNote[i]]--;
+            } else{
                 return false;
             }
         }
         return true;
     }
 };
+        
