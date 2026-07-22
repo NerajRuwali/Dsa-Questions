06e178a87bdc0fef8f1b1938e48a2649bd935ffa
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-07-22 22:10:36 +0530

    Time: 0 ms (100%), Space: 14.9 MB (38.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,6 @@ public:
         unordered_map<int , int> mp;
         for(int i = 0; i< nums.size(); i++){
             int remaining = target - nums[i];
-
             if(mp.find(remaining)!= mp.end()){
                 return {mp[remaining],i};
             }
