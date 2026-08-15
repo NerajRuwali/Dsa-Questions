5582f0ed10ad9428162ef2aebadc4cd8b8307a1e
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-08-15 11:21:09 +0530

    Time: 23 ms (78.12%), Space: 31.6 MB (39.12%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
 
-    int longestPalindromeSubseq(string s) {
-
+    int minInsertions(string s) {
         int n = s.size();
+        vector<vector<int>> dp( n , vector<int>(n , 0));
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = i+1 ; j < n ; j++){
 
-        for(int i = 0; i < n; i++) {
-            dp[i][i] = 1;
-        }
-
-        for(int i = n - 1; i >= 0; i--) {
-
-            for(int j = i + 1; j < n; j++) {
-
-                if(s[i] == s[j]) {
-
-                    dp[i][j] = 2 + dp[i+1][j-1];
-                }
-                else {
-
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i+1][j] , dp[i][j-1]);
                 }
             }
         }
-
         return dp[0][n-1];
-    }
-
-    int minInsertions(string s) {
-
-        int lps = longestPalindromeSubseq(s);
-
-        return s.size() - lps;
     }
 };
