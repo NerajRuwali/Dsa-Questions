881da334acc09976301cbdf4f0f5af5ae0135cee
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-08-17 07:23:14 +0530

    Time: 0 ms (100%), Space: 12.1 MB (86.34%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,8 +3,7 @@ public:
     int hIndex(vector<int>& citations) {
         
         sort(citations.begin(), citations.end());
-        
-        int n = citations.size();
+         int n = citations.size();
         
         for(int i = 0; i < n; i++){
             
