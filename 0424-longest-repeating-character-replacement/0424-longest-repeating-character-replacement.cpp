87e9c2b724bf1931ac9f05090d33d5336a0da192
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-08-20 10:55:16 +0530

    Time: 11 ms (28.85%), Space: 11 MB (19.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> freq(26, 0);
-        int left = 0, maxFreq = 0, maxLen = 0;
-
-        for (int right = 0; right < s.size(); right++) {
-
-            freq[s[right] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[right] - 'A']);
-            while ((right - left + 1) - maxFreq > k) {
-                freq[s[left] - 'A']--;
-                left++;
+        int ans = 0;
+        unordered_set<char>st(s.begin(), s.end());
+        for(char ch : st){
+            int count = 0;
+            int left = 0;
+            for(int right = 0 ; right < s.size(); right++){
+                if(s[right]== ch){
+                    count++;
+                }
+                while((right-left+1) - count>k){
+                    if(s[left]==ch){
+                        count--;
+                    }
+                    left++;
+                }
+                ans = max(ans , right - left +1 );
             }
-
-            maxLen = max(maxLen, right - left + 1);
         }
-
-        return maxLen;
+        return ans;
     }
 };
+
+
+
+        
