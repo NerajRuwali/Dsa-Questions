5b5db38816552940a402aaae41ba69fb09a0f7eb
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-08-20 06:43:07 +0530

    Time: 3 ms (27.77%), Space: 97.5 MB (25.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,18 +4,16 @@ public:
         int minPrice = INT_MAX;
         int maxProfit = 0;
 
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < minPrice) {
+        for(int i = 0 ; i< prices.size(); i++){
+            if(prices[i]< minPrice){
                 minPrice = prices[i];
+
             }
-
             int profit = prices[i] - minPrice;
-
-            if (profit > maxProfit) {
+            if(profit>maxProfit){
                 maxProfit = profit;
             }
         }
-
         return maxProfit;
     }
 };
