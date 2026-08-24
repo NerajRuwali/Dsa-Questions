4f844301b0702c339fdeae5ac07b8464be3bf1cf
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-08-24 22:23:54 +0530

    Time: 5 ms (20.16%), Space: 8.6 MB (54.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,15 +5,10 @@ public:
 
         while (x != 0) {
             int digit = x % 10;
-
-  
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7))
+           if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7))
                 return 0;
-
-           
-            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8))
+           if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8))
                 return 0;
-
             ans = ans * 10 + digit;
             x /= 10;
         }
