1ec3b9255d6708f3183e4ab166dddc65d61cc9b5
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-09-04 22:55:56 +0530

    Time: 312 ms (12.41%), Space: 81.2 MB (19.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,8 +4,8 @@ public:
         unordered_set<char> st;
         int left = 0;
         int longest = 0;
-        for(int right = 0 ;right < s.size(); right++){
-            while(st.find(s[right])!= st.end()){
+        for(int right = 0; right <s.size(); right++){
+            while(st.find(s[right]) != st.end()){
                 st.erase(s[left]);
                 left++;
             }
@@ -17,15 +17,4 @@ public:
 };
 
 
-
-
-
-
-
-
-
-
-
-
-     
-        
+       
