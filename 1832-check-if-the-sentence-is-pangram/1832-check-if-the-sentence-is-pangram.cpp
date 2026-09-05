26ef7c88f240059e85009cc888d1b5726b66489f
Neeraj Ruwali <nanuruwali00@gmail.com>
2026-09-05 22:19:26 +0530

    Time: 1 ms (16.78%), Space: 8.8 MB (60.55%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int freq[26] = {0};
+
+        for(char ch : sentence){
+            freq[ch - 'a'] = 1;
+
+
+        }
+        for(int i = 0; i<26 ; i++){
+            if(freq[i] == 0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
